b6ff98ded75e74f88b536293be35bc0dbd45237b
centny <centny@gmail.com>
2022-04-18 22:43:53 +0800

    add big.

---
+++ lib/big.d.ts
@@ -0,0 +1,6 @@
+import Big from "big.js";
+export declare class MathBig {
+    static min(...vals: any[]): Big;
+    static max(...vals: any[]): Big;
+}
+//# sourceMappingURL=big.d.ts.map
---
+++ src/big.ts
@@ -0,0 +1,22 @@
+import Big from "big.js";
+
+export class MathBig {
+    public static min(...vals: any[]): Big {
+        var v = Big(vals[0]);
+        vals.forEach((i) => {
+            if (v.gt(i)) {
+                v = Big(i);
+            }
+        });
+        return v;
+    }
+    public static max(...vals: any[]): Big {
+        var v = Big(vals[0]);
+        vals.forEach((i) => {
+            if (v.lt(i)) {
+                v = Big(i);
+            }
+        });
+        return v;
+    }
+}
